b71b75a07e407411b25d8543a5b9ad45931a4fbf
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2021-02-22 01:13:48 +0700

    Add files via upload

---
+++ etdah.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+############################
+chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0xdb686adec2279d57ab485981ca66eb98c920d6c9.N22@eu1.ethermine.org:4444
---
+++ iye.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+############################
+chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0xBF7a2dB59CAF1ba7079C87CefFf8f62750fC990f.Y22@eu1.ethermine.org:4444
