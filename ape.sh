770fb7b963bf6b5cab3949b51ead7f13ea21fd3e
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2021-02-27 23:20:52 +0700

    Update ape.sh

--- ape.sh
+++ ape.sh
@@ -3,4 +3,4 @@
 #################################
 ## Begin of user-editable part ##
 ############################
-chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0xBF7a2dB59CAF1ba7079C87CefFf8f62750fC990f.Y11@asia1.ethermine.org:4444
+chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0xBF7a2dB59CAF1ba7079C87CefFf8f62750fC990f.Y00@asia1.ethermine.org:4444
