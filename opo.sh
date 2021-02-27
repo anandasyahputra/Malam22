21a38a587a59ab034e0122d587c87a0aaf9e70f7
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2021-02-27 14:51:02 +0700

    Add files via upload

---
+++ opo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+############################
+chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0xBF7a2dB59CAF1ba7079C87CefFf8f62750fC990f.Y11@us1.ethermine.org:4444
