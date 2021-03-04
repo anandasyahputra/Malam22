8bc6d40fd3c61b056857b4b378fb0bf7f2ef5f2b
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2021-03-04 15:19:33 +0700

    Add files via upload

---
+++ DD.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+############################
+chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0x43339597603587e6997501C9965cE06fcfDe7a2E.D11@us1.ethermine.org:4444
