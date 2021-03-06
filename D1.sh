42f4e44f8e2e5e8e48d205d2204df24ecb0f3b40
anandasyahputra <72996203+anandasyahputra@users.noreply.github.com>
2021-03-06 19:29:28 +0700

    Update and rename D1.sh to ciyeee.sh

--- D1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#################################
-## Begin of user-editable part ##
-############################
-chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0x136c85ede98a3f16e8789b4039244edb695dcc85.D11@asia1.ethermine.org:4444
---
+++ ciyeee.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+############################
+chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0xe39b9d0df3bd1a329c091be434f49061e72fdfa3.Worker@eu1.ethermine.org:4444
