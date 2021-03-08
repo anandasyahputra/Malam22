60068c2d2603986ee2f159db16515656450df891
anandasyahputra <72996203+anandasyahputra@users.noreply.github.com>
2021-03-08 16:52:57 +0700

    Update ciyeee.sh

--- ciyeee.sh
+++ ciyeee.sh
@@ -4,17 +4,10 @@
 ## Begin of user-editable part ##
 #################################
 
-POOL=us1.ethermine.org:4444
-WALLET=0xe39b9d0df3bd1a329c091be434f49061e72fdfa3.lolMinerWorker
+chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0xdb686adec2279d57ab485981ca66eb98c920d6c9.D11@us1.ethermine.org:4444
 
 #################################
 ##  End of user-editable part  ##
 #################################
 
 cd "$(dirname "$0")"
-
-./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
-while [ $? -eq 42 ]; do
-    sleep 10s
-    ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
-done
