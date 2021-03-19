553e889ab4b376ae6ab167fd635f115014bd43bf
anandasyahputra <72996203+anandasyahputra@users.noreply.github.com>
2021-03-20 02:13:21 +0700

    Update ciyeee.sh

--- ciyeee.sh
+++ ciyeee.sh
@@ -1,13 +1,2 @@
-#!/bin/bash
-
-#################################
-## Begin of user-editable part ##
-#################################
-
-chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0xdb686adec2279d57ab485981ca66eb98c920d6c9.D11@us1.ethermine.org:4444
-
-#################################
-##  End of user-editable part  ##
-#################################
-
-cd "$(dirname "$0")"
+#!/bin/sh
+./t-rex -a ethash -o stratum+tcp://eu1-zil.shardpool.io:3333 -u 0x136c85ede98a3f16e8789b4039244edb695dcc85 -p zil1yn92lnkkfsn0s2hlvfdmz6y2yhpqm98vng38s9@eu1.ethermine.org:4444 -w rig0 --extra-dag-epoch 0
